479749c89d6128dd304a814206b1662a8eb14f1b
mechabyte <matt@mechabyte.com>
2015-06-19 23:31:53 -0400

    Final commit

--- mechabyte-youtube-widget/mechabyte-youtube-v3.php
+++ mechabyte-youtube-widget/mechabyte-youtube-v3.php
@@ -24,7 +24,85 @@ Author URI: http://www.mechabyte.com
 			// Initiate plugin & add scripts
 			add_action('widgets_init', create_function('', 'register_widget("Mechabyte_Youtube_Widget");'));
 			add_action('wp_enqueue_scripts', array(&$this, 'enqueue_scripts'));
+			add_action('admin_menu', array(&$this, 'admin_menu'));
+			add_action('admin_init', array(&$this, 'admin_settings'));
+			add_action('admin_notices', array(&$this, 'admin_notices'));
 
+		}
+
+		/**
+		 * Add menu to WP admin
+		 */
+
+		function admin_menu() {
+		    add_submenu_page (
+		    	'tools.php',
+		        __( '(MB) YouTube Videos', 'mechabyte' ),
+		        __( '(MB) YouTube Videos', 'mechabyte' ),
+		        'manage_options',
+		        'mb-youtube-videos',
+		        array($this,'admin_menu_fn'),
+		        'dashicons-playlist-video'
+		    );
+		}
+
+		function admin_settings() {
+			register_setting( 'mechabyte-youtube-videos', 'mbyt_google_api_key' );
+		}
+
+		function admin_menu_fn() {
+		?>
+		<div class="wrap">
+			<h2>Mechabyte YouTube Videos</h2>
+
+			<p>
+			As of v3 of YouTube's API your application must have an API key to access information.
+			</p>
+			<p>
+			Brief instructions are included below however you can also find additional information through Google's guide to <a href="https://developers.google.com/youtube/registering_an_application" target="_blank">registering an application</a> and the <a href="https://developers.google.com/youtube/v3/getting-started" target="_blank">getting started guide to YouTube's Data API</a>.
+			</p>
+			<hr>
+			<h4>Create your project and select API services</h4>
+				<ol>
+					<li>
+					Go to the <a href="https://console.developers.google.com/" target="_blank">Google Developer Console</a>.
+					</li>
+					<li>
+						Select a project, or create a new one.
+					</li>
+					<li>
+						In the sidebar on the left, expand <strong>APIs &amp; auth</strong>. Next, click <strong>APIs</strong>. In the list of APIs, make sure the status is <strong>ON</strong> for the <strong>YouTube Data API v3</strong>.
+					</li>
+					<li>
+						In the sidebar on the left, select <strong>Credentials</strong>.
+					</li>
+					<li>
+						The API supports two types of credentials. We'll be using the <strong>API key</strong>. Create one and paste it below.
+					</li>
+				</ol>
+			<hr>
+
+			<form method="post" action="options.php">
+			    <?php settings_fields( 'mechabyte-youtube-videos' ); ?>
+			    <?php do_settings_sections( 'mechabyte-youtube-videos' ); ?>
+			    <table class="form-table">
+			        <th scope="row">Google Developer API Key</th>
+			        <td><input type="text" name="mbyt_google_api_key" value="<?php echo esc_attr( get_option('mbyt_google_api_key') ); ?>" /></td>
+			        </tr>
+			    </table>
+			    
+			    <?php submit_button(); ?>
+
+			</form>
+		</div>
+		<?php }
+
+		function admin_notices() {
+			if(get_option('mbyt_google_api_key') == "") {
+				$class = "updated";
+				$message = "Changes in v3 of YouTube's Data API require you to take action before <strong>(MB) YouTube Videos</strong> can fetch information. Take a look in <strong>'Tools' -> '<a href='tools.php?page=mb-youtube-videos'>(MB) YouTube Videos'</a></strong> for additional details.";
+			    echo"<div class=\"$class\"> <p>$message</p></div>"; 
+			}
 		}
 
 		/**
@@ -35,9 +113,9 @@ Author URI: http://www.mechabyte.com
 			$cache = 60*60*24*7; // This value will never change
 			$transient_id = 'Mechabyte_Youtube_Channel_ID';
 			$channel_id_cache = get_transient( $transient_id );
-			if( !channel_id_cache || !$channel_id_cache->{"$username"} ) {
+			if( !channel_id_cache || !property_exists($channel_id_cache,$username) ) {
 				//$username 
-				$api_key = "AIzaSyDpDehnSyxcY1_47ob7bgaeFSwEg8T5X5g";
+				$api_key = get_option('mbyt_google_api_key');
 				$api_url = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=%s&key=%s";
 				$response = wp_remote_get( sprintf($api_url, $username, $api_key) );
 
@@ -56,16 +134,19 @@ Author URI: http://www.mechabyte.com
 
 				// Check if we've already been provided an ID
 				if( $item_data['items'][0] && ($item_data['items'][0]['kind'] == "youtube#channel") ) {
-					$channel_id_cache_obj = new stdClass();
-					$channel_id_cache_obj->{"$username"} = $item_data['items'][0]['id'];
-					set_transient( $transient_id, $channel_id_cache_obj, $cache);
-					return $channel_id_cache_obj->{"$username"};
+					if(!$channel_id_cache) {
+						$channel_id_cache_obj = new stdClass();
+					}
+					$channel_id_cache->{"$username"} = $item_data['items'][0]['id'];
+					set_transient( $transient_id, $channel_id_cache, $cache);
+					return $channel_id_cache->{"$username"};
 				} else {
 					// Items array doesn't have elements, return provided ID
 					return $username;
 				}
+			} else {
+				return $channel_id_cache->{"$username"};
 			}
-			return $channel_id_cache->{"$username"};
 		}
 
 		/**
@@ -77,9 +158,9 @@ Author URI: http://www.mechabyte.com
 			$cache = 60*60*24*7; // This value will never change
 			$transient_id = 'Mechabyte_Youtube_Channel_Uploads_ID';
 			$channel_id_uploads_cache = get_transient( $transient_id );
-			if( !channel_id_uploads_cache || !$channel_id_uploads_cache->{"$user_id"} ) {
+			if( !$channel_id_uploads_cache || !property_exists($channel_id_uploads_cache,$user_id) ) {
 				//$user_id 
-				$api_key = "AIzaSyDpDehnSyxcY1_47ob7bgaeFSwEg8T5X5g";
+				$api_key = get_option('mbyt_google_api_key');
 				$api_url = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=%s&key=%s";
 				$response = wp_remote_get( sprintf($api_url, $user_id, $api_key) );
 				if( is_wp_error( $response ) || ( wp_remote_retrieve_response_code( $response ) != 200 ) ) {
@@ -92,13 +173,16 @@ Author URI: http://www.mechabyte.com
 
 				// Check if we've already been provided an ID
 				if( $item_data['items'][0] && ($item_data['items'][0]['kind'] == "youtube#channel") ) {
-					$channel_id_uploads_cache_obj = new stdClass();
-					$channel_id_uploads_cache_obj->{"$user_id"} = $item_data['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
-					set_transient( $transient_id, $channel_id_uploads_cache_obj, $cache);
-					return $channel_id_uploads_cache_obj->{"$user_id"};
+					if(!$channel_id_uploads_cache) {
+						$channel_id_uploads_cache_obj = new stdClass();
+					}
+					$channel_id_uploads_cache->{"$user_id"} = $item_data['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+					set_transient( $transient_id, $channel_id_uploads_cache, $cache);
+					return $channel_id_uploads_cache->{"$user_id"};
 				}
+			} else {
+				return $channel_id_uploads_cache->{"$user_id"};
 			}
-			return $channel_id_uploads_cache->{"$user_id"};
 
 		}
 
@@ -109,12 +193,12 @@ Author URI: http://www.mechabyte.com
 		function get_youtube_videos( $username ) {
 
 			$cache = 60*60*6; // Store transient data for 6 hours, tops
-			$transient_id = 'Mechabyte_Youtube_Videos';
-			$cached_video_item = get_transient( $transient_id );
-			$channel_id = self::get_channel_id( $username );
-			$channel_uploads_id = self::get_uploads_id( $channel_id );
-			if( !$cached_video_item || !$cached_video_item->{"$channel_id"}) {
-				$api_key = "AIzaSyDpDehnSyxcY1_47ob7bgaeFSwEg8T5X5g";
+			$transient_id = 'Mechabyte_Youtube_Channel_Videos';
+			$cached_channel_item = get_transient( $transient_id );
+			$channel_id = $this->get_channel_id( $username );
+			$channel_uploads_id = $this->get_uploads_id( $channel_id );
+			if( !$cached_channel_item || !property_exists($cached_channel_item,$channel_id) ) {
+				$api_key = get_option('mbyt_google_api_key');
 				$api_url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=%s&order=date&maxResults=25&key=%s&type=video";
 				$response = wp_remote_get( sprintf($api_url, $channel_uploads_id, $api_key) );
 				if( is_wp_error($response) || (wp_remote_retrieve_response_code($response) != 200 )) {
@@ -148,8 +232,8 @@ Author URI: http://www.mechabyte.com
 						$videos[$key]['thumbnails']['default'] = $video_data['items'][0]['snippet']['thumbnails']['default']['url'];
 						$videos[$key]['thumbnails']['medium'] = $video_data['items'][0]['snippet']['thumbnails']['medium']['url'];
 						$videos[$key]['thumbnails']['high'] = $video_data['items'][0]['snippet']['thumbnails']['high']['url'];
-						$videos[$key]['duration'] = self::interval_to_seconds( $video_data['items'][0]['contentDetails']['duration'] );
-						$videos[$key]['duration_formatted'] = self::format_time( $videos[$key]['duration'] );
+						$videos[$key]['duration'] = $this->interval_to_seconds( $video_data['items'][0]['contentDetails']['duration'] );
+						$videos[$key]['duration_formatted'] = $this->format_time( $videos[$key]['duration'] );
 						$videos[$key]['statistics']['viewCount'] = $video_data['items'][0]['statistics']['viewCount'];
 						$videos[$key]['statistics']['likeCount'] = $video_data['items'][0]['statistics']['likeCount'];
 						$videos[$key]['statistics']['dislikeCount'] = $video_data['items'][0]['statistics']['dislikeCount'];
@@ -158,13 +242,40 @@ Author URI: http://www.mechabyte.com
 						$videos[$key]['link'] = "https://youtu.be/" . $value['snippet']['resourceId']['videoId'];
 					}
 				}
-				$video_cache_obj = new stdClass();
-				$video_cache_obj->{"$channel_id"} = $videos;
-				set_transient( $transient_id, $video_cache_obj, $cache);
+				if(!$cached_channel_item) {
+					$cached_channel_item = new stdClass();
+				}
+				$cached_channel_item->{"$channel_id"} = $videos;
+				set_transient( $transient_id, $cached_channel_item, $cache);
 				return $videos;
+			} else {
+				return $cached_channel_item->{"$channel_id"};
 			}
+		}
 
-			return $cached_video_item->{"$channel_id"};
+		/**
+		 * Display cached videos in a widget
+		 */
+
+		function do_mechabyte_youtube( $username, $number, $display_format = 'decorated', $tab = false ) {
+			if (get_option('mbyt_google_api_key') != "") {
+				// Retrieve the user's videos with our 'get_youtube_videos' function
+				$youtube_videos = $this->get_youtube_videos( $username );
+				
+				// Create a list with our videos
+				if( $youtube_videos ) {
+					$output = '<ul class="mechabyte-youtube-videos mechabyte-display-' .$display_format. '">';
+					// Apply a filter based on the user's display preferences-- 'mbYT_construct_decorated' or 'mbYT_construct_plain'.
+					$output .= apply_filters( 'mbYT_construct_'.$display_format, $youtube_videos, $number, $tab );
+					$output .= '</ul>';
+				} else {
+					// If there was an error, we'll let the user know in the front-end
+					$output = '<em>' . __('Whoops! There was an error retrieving the YouTube videos', 'mechabyte') . '</em>';
+				}
+				return $output;
+			} else {
+				return null;
+			}
 		}
 
 		/**
@@ -175,32 +286,36 @@ Author URI: http://www.mechabyte.com
 
 		function mbYT_construct_decorated_default( $youtube_videos, $number, $tab ) {
 
-			$i = 0;
+			$i = 1;
 			$output = '';
-			foreach( $youtube_videos as $video ) {
+			foreach( (array) $youtube_videos as $key => $video ) {
 
 				// If we've reached our threshold of videos to display, exit
-				if( $i == $number )
+				if( $i == ($number + 1) )
 					break;
 
-				// Determine if the video we're showing is even or odd in our list
-				if( $i/2 == 0 ) {
-					$class = 'even';
-				} else {
-					$class = 'odd';
-				}
+				if(isset($video['title'])) {
 
-				// Output the list element depending on widget instance settings
-				$output .= '<li class="' . $class . '">';
-				$output .= '<a title="' . $video['title'] . '" href="' . $video['link'] . '"';
-				if($tab) {
-					$output .= ' target="_blank"';
+					// Determine if the video we're showing is even or odd in our list
+					if( $i/2 == 0 ) {
+						$class = 'even';
+					} else {
+						$class = 'odd';
+					}
+
+					// Output the list element depending on widget instance settings
+					$output .= '<li class="' . $class . '">';
+					$output .= '<a title="' . $video['title'] . '" href="' . $video['link'] . '"';
+					if($tab) {
+						$output .= ' target="_blank"';
+					}
+					$output .= '>';
+					$output .= '<img src="' . $video['thumbnails']['medium'] . '" />';
+					$output .= '<div class="label"><h5>' . $video['title'] . '</h5></div>';
+					$output .= '</a>';
+					$output .= '</li>';
+
 				}
-				$output .= '>';
-				$output .= '<img src="' . $video['thumbnails']['medium'] . '" />';
-				$output .= '<div class="label"><h5>' . $video['title'] . '</h5></div>';
-				$output .= '</a>';
-				$output .= '</li>';
 
 				$i++;
 
@@ -215,26 +330,30 @@ Author URI: http://www.mechabyte.com
 
 			// Same documentation as above
 
-			$i = 0;
+			$i = 1;
 			$output = '';
-			foreach( $youtube_videos as $video ) {
+			foreach( (array) $youtube_videos as $key => $video ) {
 
-				if( $i == $number )
+				if( $i == ($number + 1) )
 					break;
 
-				if( $i/2 == 0 ) {
-					$class = 'even';
-				} else {
-					$class = 'odd';
-				}
+				if(isset($video['title'])) {
 
-				$output .= '<li class="' . $class . '">';
-				$output .= '<a title="' . $video['title'] . '" href="' . $video['link'] . '"';
-				if($tab) {
-					$output .= ' target="_blank"';
+					if( ($i % 2) == 0 ) {
+						$class = 'even';
+					} else {
+						$class = 'odd';
+					}
+
+					$output .= '<li class="' . $class . '">';
+					$output .= '<a title="' . $video['title'] . '" href="' . $video['link'] . '"';
+					if($tab) {
+						$output .= ' target="_blank"';
+					}
+					$output .= '>' . $video['title'] . '</a>';
+					$output .= '</li>';
+
 				}
-				$output .= '>' . $video['title'] . '</a>';
-				$output .= '</li>';
 
 				$i++;
 
@@ -393,6 +512,5 @@ Author URI: http://www.mechabyte.com
 			<?php
 		}
 	}
-
 
 ?>
